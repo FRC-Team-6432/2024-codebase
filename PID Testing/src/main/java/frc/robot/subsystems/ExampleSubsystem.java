40bd6a186e027ab47acea244cd4690dd8c44ad1e
Emily Bartlett <bartlette@student.krb.nsw.edu.au>
2024-03-07 16:45:01 +1100

    change ID

--- PID Testing/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ PID Testing/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -19,8 +19,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   DutyCycleEncoder encoder = new DutyCycleEncoder(4);
-  WPI_TalonSRX motorLeft = new WPI_TalonSRX(1);
-  WPI_TalonSRX motorRight = new WPI_TalonSRX(2);
+  WPI_TalonSRX motorLeft = new WPI_TalonSRX(2);
+  WPI_TalonSRX motorRight = new WPI_TalonSRX(1);
   // WPI_TalonSRX motorTalon = new WPI_TalonSRX(2);
   // WPI_TalonSRX motorTalon2 = new WPI_TalonSRX(3);
 
