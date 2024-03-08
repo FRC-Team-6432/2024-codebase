0657c02a1407b69d00ae478d8591c6edd92ccdc4
elliotWinn <elliot.winn6@gmail.com>
2024-03-09 10:55:17 +1100

    added get angle to goal code

--- FirstRobotCodeTest/src/main/java/frc/robot/subsystems/ArmUpdated.java
+++ FirstRobotCodeTest/src/main/java/frc/robot/subsystems/ArmUpdated.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.XboxController;
@@ -24,7 +25,16 @@ public class ArmUpdated extends SubsystemBase {
   final int RIGHT_MOTOR_ID = 1;
   final int ENCODER_CHANNEL = 0;
 
-  final double OFFSET_ENCODER = 0; // Encoder value when arm is at 0 deg to horizontal
+  final double LIMELIGHT_MOUNT_HEIGHT = 1270;
+  final double ARM_BASE_HEIGHT = 1270;
+  final double ARM_LENGTH = 582;
+
+  final double LIMELIGHT_MOUNT_ANGLE = 90 - Math.toDegrees(Math.atan(37/54));
+  final double SHOOTER_ARM_ANGLE = 110;
+
+  final double INTAKE_ENCODER_VALUE = 0.143; // Encoder value when arm is at 0 deg to horizontal ie intake position
+  final double MAX_ENCODER_VALUE = INTAKE_ENCODER_VALUE+0.3; // Max arm is allowed to go back
+
   WPI_TalonSRX motorLeft = new WPI_TalonSRX(LEFT_MOTOR_ID);
   WPI_TalonSRX motorRight = new WPI_TalonSRX(RIGHT_MOTOR_ID);
   DutyCycleEncoder boreEncoder = new DutyCycleEncoder(ENCODER_CHANNEL);
@@ -43,8 +53,35 @@ public class ArmUpdated extends SubsystemBase {
     table = NetworkTableInstance.getDefault().getTable("limelight");
   }
 
+  public double getAngleToGoal() {
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    
+    double tagHeight = 1367;
+
+    double heightFromLimelight = tagHeight-LIMELIGHT_MOUNT_HEIGHT;
+    double heightFromArmBase = tagHeight - ARM_BASE_HEIGHT;
+
+    double angleFromLimelight = LIMELIGHT_MOUNT_ANGLE + y;
+    
+    double horizontalDistance = heightFromLimelight / Math.tan(Math.toRadians(angleFromLimelight));
+
+    double armBaseTargetDistance = Math.sqrt(heightFromArmBase*heightFromArmBase + horizontalDistance*horizontalDistance);
+    double angleFromArmBase = Math.toDegrees(Math.atan(heightFromArmBase/horizontalDistance));
+
+    // double limelightTargetDistance = heightFromLimelight / Math.sin(Math.toRadians(angleFromLimelight));
+
+    double theta = Math.toDegrees(Math.asin((ARM_LENGTH*Math.sin(Math.toRadians(180-SHOOTER_ARM_ANGLE)))/armBaseTargetDistance));
+
+    double armAngle = 180 - angleFromArmBase + theta - SHOOTER_ARM_ANGLE;
+    return armAngle;
+  }
+
   public void setArmToAngle(double angleDeg) {
-    double position = angleDeg/360 + OFFSET_ENCODER;
+    double position = angleDeg/360 + INTAKE_ENCODER_VALUE;
     motorLeft.set(ControlMode.Position, pidController.calculate(boreEncoder.getDistance(), position));  
   }
 
