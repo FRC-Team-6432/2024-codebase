2bb18be66e7d7f74c5ff46c635609766024edbe8
Julia <juliamallam@stcatherines.nsw.edu.au>
2024-03-12 18:51:10 +1100

    auto

---
+++ FirstRobotCodeTest/src/main/java/frc/robot/autos/AutoShoot.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autos;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj.Timer;
+
+public class AutoShoot extends Command {
+  /** Creates a new AutoShoot. */
+  Intake intake;
+  Timer timer = new Timer();
+  private boolean finish = false;
+  public AutoShoot(Intake intoke) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    intake = intoke;
+    addRequirements(intake);
+    
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    timer.reset();
+    timer.start();
+    while (timer.get() < 5){
+      intake.autoyeet();
+    }
+    finish = true;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return finish;
+  }
+}
---
+++ XAimingTest/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.time.StopWatch;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+  public DigitalInput sensor;
+  private WPI_VictorSPX intakeMotor = new WPI_VictorSPX(5);
+  private TalonFX shooterLeft = new TalonFX(4);
+  private TalonFX shooterRight = new TalonFX(3);
+  private StopWatch timer = new StopWatch();
+  
+  public Intake() {
+    sensor = new DigitalInput(0);
+    
+  }
+
+  public void yoink(XboxController controller){
+    SmartDashboard.putBoolean("intake button: ", controller.getLeftBumper());
+    SmartDashboard.putBoolean("intake", false);
+    if(controller.getLeftBumper()){
+      if(!sensor.get()){
+        intakeMotor.set(0);
+      }
+      else{
+        SmartDashboard.putBoolean("intake", true);
+        intakeMotor.set(0.4);
+      }
+    }
+    else{
+      intakeMotor.set(0);
+    }
+  }
+
+  public void yeet(XboxController controller){
+    if (controller.getRightBumper()){
+      shooterLeft.set(-1);
+      shooterRight.set(1);
+      intakeMotor.set(0.2);
+    }
+    else{
+      shooterLeft.set(0);
+      shooterRight.set(0);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putBoolean("sensor", !sensor.get());
+    
+    // This method will be called once per scheduler run
+  }
+}
