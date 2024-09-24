0b38c9101413c04483a6359a2bba3083248cd0eb
elliotWinn <elliot.winn6@gmail.com>
2024-09-24 17:34:43 +1000

    switch display

---
+++ intake/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  DigitalInput buttonSwitch = new DigitalInput(6);
+
+  /** Creates a new Intake. */
+  public Intake() {
+
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putBoolean("switch", buttonSwitch.get());
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
